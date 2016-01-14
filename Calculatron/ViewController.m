42eab80fce7b06eb99fb20888d94544bb0520c84
Matt Deuschle <mdeuschle@gmail.com>
2016-01-14 11:52:55 -0600

    Added Button

--- Calculatron/ViewController.m
+++ Calculatron/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *operandOne;
+@property (strong, nonatomic) IBOutlet UITextField *operandTwo;
+@property (strong, nonatomic) IBOutlet UILabel *operation;
 
 @end
 
@@ -16,12 +19,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onOperandPressed:(UIButton *)sender {
 }
 
 @end
